8bb48e1add07ef808dfa0b36952b35462302ba36
tetanic <tetanicfish2268@outlook.jp>
2024-05-12 22:40:53 +0900

    範囲の定数化

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,36 @@ use std::cmp::Ordering;
 use std::env;
 use rand::Rng;
 
+const MIN: i32 = 1;
+const MAX: i32 = 100;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let arg = args.get(1).unwrap().clone();
     let mut uragane = false;
 
-    if arg == String::from("uragane") {
-        uragane = true;
+    // コマンドライン引数が "uragane" の場合、チートモードにする
+    if let Some(arg) = args.get(1) {
+        if arg == "uragane" {
+            uragane = true;
+        }
     }
 
     println!("数を当ててみて!");
 
-    let secret_number = rand::thread_rng().gen_range(1..101);
+    //数の生成
+    let secret_number = rand::thread_rng().gen_range(MIN..=MAX);
     if uragane {
         println!("ここだけの話、秘密の数字は{}だよ!", secret_number);
     }
 
     loop {
-        println!("予想を入力しよう");
+        println!("予想を入力しよう 範囲は{}から{}だよ", MIN, MAX);
 
+        //入力
         let mut guess = String::new();
         io::stdin().read_line(&mut guess).expect("ごめん、なんかエラーでたわ");
 
+        //i32にキャスト
         let guess: i32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => {
@@ -33,6 +41,7 @@ fn main() {
             },
         };
 
+        //判定
         println!("あなたの予想は{}", guess);
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("もっと大きいよ!"),       //小さすぎ！
